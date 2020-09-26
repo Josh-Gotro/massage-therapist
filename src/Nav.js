2f0d9319b1bdd3b46a6553126b4e7503ca2456d7
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-09-26 12:58:23 -0500

    add toggle to nav styling, fix background image to full view

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CSS/Nav.css';
 
 
 const Nav = () => {
     const contact = "https://www.schedulista.com/schedule/mantismassage1/choose_time?provider_id=1073881022&service_id=1073958799"
+    const [viewToggle, setViewToggle] = useState(true);
+
+    function viewNav(){
+        setViewToggle(prev => !prev)
+        console.log(viewToggle)
+    }
 
     return (
         <div className="NavContainer">
@@ -13,12 +19,12 @@ const Nav = () => {
                     "No worries, I got your back." <br/>
                     - Ghandi
                 </div>
-            </div>
 
+            </div>
             <div className="NavBar">
-                <div>RB</div>
+                <div onClick={viewNav}>RB</div>
                 <div>Technique</div>
-                <a href={contact} target="_blank">Book a Massage</a> 
+                <a href={contact} target="_blank">Book a Massage</a>
                 <div>Contact</div>
                 <div> Soc   Soc</div>
             </div>
